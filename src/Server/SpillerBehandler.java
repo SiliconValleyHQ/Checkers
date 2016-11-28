b99a2a15eae69486eae6bed5b577bb5eee7a9be3
Bror <brorbrurberg@me.com>
2016-11-28 11:20:54 +0100

    fått server del til å fungere
    Skriv «telnet 127.0.0.1 28702» i terminalen

--- src/Server/SpillerBehandler.java
+++ src/Server/SpillerBehandler.java
@@ -17,18 +17,10 @@ public class SpillerBehandler extends Thread {
     public void run() {
         try {
             PrintStream output = new PrintStream(socket.getOutputStream());
-            output.println("<h3>melding mottatt</h3>");
-            output.close();
+            output.println("itzzz workzzz");
         } catch (Exception e) {
             System.err.print("Melding ikke motatt");
         }
     }
-    /*public void run(){
-        try {
-            PrintStream output = new PrintStream(socket.getOutputStream());
-            output.println("<h3>Fikk meldingen</h3>");
-            output.close();
-        }
-        catch( Exception e ){}
-    }*/
+
 }
