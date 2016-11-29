f5109dd231733d00bb8079bc2633657e6a10fa41
Bror <brorbrurberg@me.com>
2016-11-29 11:29:47 +0100

    ny type server
    Du kan nå skrive inn server og port i konsollen for å bestemme hvor en
    skal connecte

--- src/Server/Server.java
+++ src/Server/Server.java
@@ -2,6 +2,7 @@ package server;
 
 import java.io.*;
 import java.net.*;
+import java.util.Scanner;
 
 /**
  * Created by Bror on 18.11.2016.
@@ -9,7 +10,54 @@ import java.net.*;
 
 public class Server {
 
-    public Server() throws IOException{
+    private String ip = "localhost";
+    private int port = 28702;
+    private Scanner scanner = new Scanner(System.in);
+    private Thread thread;
+
+    private Socket socket;
+    private DataOutputStream dos;
+    private DataInputStream dis;
+
+    private ServerSocket serverSocket;
+
+    public Server() {
+        System.out.println("Skriv inn ip-adressen");
+        ip = scanner.nextLine();
+        System.out.println("skriv inn en port");
+        port = scanner.nextInt();
+        while (port < 1 || port > 66666) {
+            System.out.println("Porten du skrev inn er ikke en gyldig port mellom 1 og 66666");
+            port = scanner.nextInt();
+        }
+        serverSocket = null;
+        try {
+            serverSocket = new ServerSocket(port);
+            System.out.println("Venter på spillere på port " + port + "...");
+            while ((socket = serverSocket.accept())!=null) {
+                System.out.println("Ny spiller forespørsel");
+                new SpillerBehandler(socket).start();
+            }
+        } catch (Exception e) {
+            System.err.println("Port " + port + " er opptatt...");
+            e.printStackTrace();
+        }
+
+    }
+
+    private void lytterEtterAnnenServer() {
+        Socket socket = null;
+        try {
+            socket = serverSocket.accept();
+            dos = new DataOutputStream(socket.getOutputStream());
+            dis = new DataInputStream(socket.getInputStream());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /*public Server() throws IOException{
         ServerSocket serverSocket = null;
         int port = 28702;
         try {
@@ -24,6 +72,6 @@ public class Server {
             System.err.println("Ingen forbindelse ved port " + port + "...");
             e.printStackTrace();
         }
-    }
+    }*/
 
 }
